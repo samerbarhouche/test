74bf3c599eb1440475b513f32049236a8148359b
Samer Barhouche <36796508+samerbarhouche@users.noreply.github.com>
2021-02-22 23:24:53 +0200

    Update script1.ps1

--- script1.ps1
+++ script1.ps1
@@ -1,13 +1,1 @@
-# Set directory for installation - Chocolatey does not lock
-# down the directory if not the default
-$InstallDir='C:\ProgramData\chocoportable'
-$env:ChocolateyInstall="$InstallDir"
-
-# If your PowerShell Execution policy is restrictive, you may
-# not be able to get around that. Try setting your session to
-# Bypass.
-Set-ExecutionPolicy Bypass -Scope Process -Force;
-
-# All install options - offline, proxy, etc at
-# https://chocolatey.org/install
-iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy RemoteSigned; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
