2b8bd73da1069575af28c03df6ff29e3a2133adb
Samer Barhouche <36796508+samerbarhouche@users.noreply.github.com>
2021-02-22 21:39:03 +0200

    Update script1.ps1

--- script1.ps1
+++ script1.ps1
@@ -1,3 +1,3 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
-choco install mssqlserver2014express mssqlserver2014-sqlexpradv googlechrome notepadplusplus.install
+
